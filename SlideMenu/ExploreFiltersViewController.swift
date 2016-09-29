b4538bd4e7aae1162ddead31979f83bf23cd14fa
Lauren N. Roth <laurenroth@gmail.com>
2016-09-29 16:53:28 -0400

    initial commit

---
+++ SlideMenu/ExploreFiltersViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreFiltersViewController.swift
+//  SlideMenu
+//
+//  Created by Lauren Nicole Roth on 9/27/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ExploreFiltersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        print("hello from explore filters")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
